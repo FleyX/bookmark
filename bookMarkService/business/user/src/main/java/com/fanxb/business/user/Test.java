65e1ae789f841a6f9b67cdc4b59ac992184d786a
fanxb <fanxb@szinfinova.com>
2019-07-04 17:20:02 +0800

    ✨ Feat: [后台]初始化

---
+++ bookMarkService/business/user/src/main/java/com/fanxb/business/user/Test.java
@@ -0,0 +1,12 @@
+package com.fanxb.business.user;
+
+/**
+ * 类功能简述：
+ * 类功能详述：
+ *
+ * @author fanxb
+ * @date 2019/7/3 20:16
+ */
+public class Test {
+    public int id;
+}
---
+++ bookMarkService/business/web/src/main/java/com/fanxb/business/web/Application.java
@@ -0,0 +1,18 @@
+package com.fanxb.business.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能简述：
+ * 类功能详述：
+ *
+ * @author fanxb
+ * @date 2019/7/3 20:20
+ */
+@SpringBootApplication(scanBasePackages = "com.fanxb")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
