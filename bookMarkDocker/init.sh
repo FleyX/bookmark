e964eaf80eeaf3a127a73fc3f4f2f7e3f2371c1d
fanxb <fanxb@szinfinova.com>
2019-07-16 15:55:05 +0800

    🐛 Fix: [部署]:修改init.sh换行符

--- bookMarkDocker/init.sh
+++ bookMarkDocker/init.sh
@@ -4,7 +4,7 @@ echo $base
 
 # 用于前后端打包
 
-#docker run -it --rm --name buildBookmark -v $base/../front:/opt/front registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build"
+docker run -it --rm --name buildBookmark -v $base/../front:/opt/front registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build"
 
 docker run -it --rm --name buildBookmark  -v $base/mavenRep:/opt/mavenRep -v $base/../bookMarkService:/opt/backend registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/backend && mvn clean install && echo over"
 
