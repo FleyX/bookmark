a386cbef87c0b21166b787bb6a680a0ab72c5dfa
fanxb <fanxb.tl@gmail.com>
2019-07-16 03:49:22 -0400

    [部署]:修改init.sh

--- bookMarkDocker/init.sh
+++ bookMarkDocker/init.sh
@@ -1,6 +1,11 @@
 #/bin/bash
+base=$(cd "$(dirname "$0")";pwd)
+echo $base
 
 # 用于前后端打包
-docker run -it --rm --name buildBookmark -v ../front:/opt/front -v ../bookMarkService:/opt/backend registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build && cd /opt/backend && mvn package"
+
+#docker run -it --rm --name buildBookmark -v $base/../front:/opt/front registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build"
+
+docker run -it --rm --name buildBookmark  -v $base/mavenRep:/opt/mavenRep -v $base/../bookMarkService:/opt/backend registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/backend && mvn clean install && echo over"
 
 echo "打包完毕，export MYSQL_PASS=xxx 设置环境变量后执行docker-compose up -d即可"
