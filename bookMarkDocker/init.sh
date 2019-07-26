aa5e29d1adcb64cc2496452bd2730c295dcb2333
fanxb <fanxb.tl@gmail.com>
2019-07-26 17:53:09 +0800

    🚀 Deploy: [部署]:设置maven编码为utf8

--- bookMarkDocker/init.sh
+++ bookMarkDocker/init.sh
@@ -6,6 +6,6 @@ echo $base
 
 docker run -it --rm --name buildBookmark -v $base/../front:/opt/front registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build"
 
-docker run -it --rm --name buildBookmark  -v $base/mavenRep:/opt/mavenRep -v $base/../bookMarkService:/opt/backend registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/backend && mvn clean install && echo over"
+docker run -it --rm --name buildBookmark  -v $base/mavenRep:/opt/mavenRep -v $base/../bookMarkService:/opt/backend -e MAVEN_OPTS="-Xms256m -Xmx512m -Dfile.encoding=UTF-8" registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v1 sh -c "cd /opt/backend && mvn clean install && echo over"
 
 echo "打包完毕，export MYSQL_PASS=xxx 设置环境变量后执行docker-compose up -d即可"
