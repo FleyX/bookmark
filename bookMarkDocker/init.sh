67ffccba54e19a12781a8fa9bfb97cb4c42a5108
fanxb <fanxb.tl@gmail.com>
2019-07-30 13:55:48 +0800

    🚀 Deploy: [部署]：在此创建es数据文件夹并设置max_map_count

--- bookMarkDocker/init.sh
+++ bookMarkDocker/init.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 base=$(cd "$(dirname "$0")";pwd)
 
+# 创建es存放数据文件夹并设置读写权限
+mkdir $path/es/data>/dev/null 2>&1
+
+chmod 777 $path/es/data
+
+sysctl -w vm.max_map_count=262144
+
 # 用于前后端打包
 
 docker run -it --rm --name buildBookmark -v $base/../front:/opt/front registry.cn-hangzhou.aliyuncs.com/fleyx/node-java-env:v2 sh -c "cd /opt/front && npm --registry https://registry.npm.taobao.org install && npm run build"
