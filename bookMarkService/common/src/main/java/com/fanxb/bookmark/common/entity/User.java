c2b865e996bde8f4f0a20fddf342dff6aef67579
fanxb <fanxb.tl@gmail.com>
2019-12-08 09:05:37 +0000

    temp

--- bookMarkService/common/src/main/java/com/fanxb/bookmark/common/entity/User.java
+++ bookMarkService/common/src/main/java/com/fanxb/bookmark/common/entity/User.java
@@ -1,7 +1,11 @@
 package com.fanxb.bookmark.common.entity;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 类功能简述：
@@ -26,4 +30,20 @@ public class User {
      * 上次更新书签时间
      */
     private long bookmarkChangeTime;
+
+    public static void main(String[] args) {
+        User user1 = new User();
+        User user2 = new User();
+        Map<User, User> map = new HashMap<>();
+        map.put(user1, user2);
+        NewUserObj obj = new NewUserObj();
+        obj.setMap(map);
+        NewUserObj newObj = JSON.parseObject(JSON.toJSONString(obj), NewUserObj.class);
+        System.out.println(newObj.getMap().get(user1));
+    }
+
+    @Data
+    public static class NewUserObj {
+        private Map<User, User> map;
+    }
 }
