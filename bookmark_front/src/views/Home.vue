d69df99a10e3fa6bd4973621cf2c8959cda6784e
fanxb <fanxb.tl@gmail.com>
2020-06-24 13:54:15 +0800

    init:初始化

---
+++ bookmark_front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    {{ count }}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapState } from "vuex";
+import * as types from "../store/type";
+
+export default {
+  name: "Home",
+  computed: {
+    ...mapState([types.COUNT])
+  },
+  components: {}
+};
+</script>
