0aac5fdc20938400dc8eb4637ca8a541c18bb216
fanxb <fleyx20@outlook.com>
2021-03-15 00:06:57 +0800

    fix:修复重复创建索引bug

--- bookMarkService/web/src/main/resources/db/migration/V16__user表改结构.sql
+++ bookMarkService/web/src/main/resources/db/migration/V16__user表改结构.sql
@@ -1,14 +1,9 @@
 alter table user
     add githubId bigint default -1 not null comment '-1说明未使用github登陆' after userId;
 
-create unique index email_index
-    on user (email);
 
 create index githubIdIndex
     on user (githubId);
 
 create index new_email_index
     on user (newEmail);
-
-create unique index username_index
-    on user (username);
