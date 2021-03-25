2db39c0b77aeadcc366b88167cca9cde0ed88e17
fanxb <fleyx20@outlook.com>
2021-03-25 17:22:10 +0800

    refactor:注册接口加校验

--- bookMarkService/business/user/src/main/java/com/fanxb/bookmark/business/user/vo/RegisterBody.java
+++ bookMarkService/business/user/src/main/java/com/fanxb/bookmark/business/user/vo/RegisterBody.java
@@ -2,6 +2,9 @@ package com.fanxb.bookmark.business.user.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 /**
  * 类功能简述： 注册表单
  * 类功能详述：
@@ -11,8 +14,13 @@ import lombok.Data;
  */
 @Data
 public class RegisterBody {
+    @NotBlank(message = "用户名不能为空")
+    @Pattern(regexp = "^\\w{1,50}$", message = "用户名长度为1-50")
     private String username;
+    @NotBlank(message = "密码不能为空")
+    @Pattern(regexp = "^\\w{6,18}$", message = "密码为6-18位组合")
     private String password;
+    @NotBlank(message = "邮箱不能为空")
     private String email;
     private String authCode;
 }
