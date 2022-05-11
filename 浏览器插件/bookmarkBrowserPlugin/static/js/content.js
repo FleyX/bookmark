d5e2b55c2823595fe9d7f2f97e2ea1357a5c582c
fanxb <fleyx20@outlook.com>
2022-05-11 14:30:38 +0800

    refactor:日志打印级别改为debug

--- 浏览器插件/bookmarkBrowserPlugin/static/js/content.js
+++ 浏览器插件/bookmarkBrowserPlugin/static/js/content.js
@@ -1,4 +1,4 @@
-console.log('注入了页面');
+console.debug('注入了页面');
 
 var bookmarkInfo = null;
 var addBlockDiv = null;
@@ -8,11 +8,11 @@ var iframe = null;
  * 接收当前注入页面传来的消息
  */
 window.addEventListener('message', function (event) {
-  console.log(event);
+  console.debug(event);
   if (event.data.code === undefined) {
     return;
   }
-  console.log('接受到网页消息:', event.data);
+  console.debug('接受到网页消息:', event.data);
   if (event.data.code === 'getBookmarkData') {
     iframe.contentWindow.postMessage({ code: "addBookmarkAction", data: bookmarkInfo }, "*");
   } else if (event.data.code === 'setToken') {
@@ -31,7 +31,7 @@ chrome.runtime.onMessage.addListener(async (data, sender, sendResponse) => {
     return;
   }
   sendResponse("ok");
-  console.log('收到消息：', data);
+  console.debug('收到消息：', data);
   if (data.code == 'setTokenOk') {
     sendToPage(data);
   } else if (data.code == 'addBookmark') {
@@ -49,13 +49,13 @@ async function addBookmark (data) {
   try {
     if (data.data.iconUrl) {
       let icon = await axios.get(data.data.iconUrl, { responseType: 'arraybuffer' });
-      console.log(JSON.stringify(new Uint8Array(icon.data)));
+      console.debug(JSON.stringify(new Uint8Array(icon.data)));
       data.data.icon = `data:` + icon.headers['content-type'] + ';base64,' + window.btoa(String.fromCharCode(...new Uint8Array(icon.data)));
     }
   } catch (error) {
     console.error(error);
   }
-  console.log("新增书签", data.data);
+  console.debug("新增书签", data.data);
   bookmarkInfo = data.data;
   addBlockDiv = document.createElement("div");
   addBlockDiv.setAttribute("style", "position:fixed;width:100%;height:100vh;z-index:100000;left:0;top:0;background:rgba(211, 211, 205, 0.8)");
@@ -73,7 +73,7 @@ async function addBookmark (data) {
 function sendToBg (data) {
   data.receiver = "background";
   chrome.runtime.sendMessage(data, response => {
-    console.log(response);
+    console.debug(response);
   });
 }
 
