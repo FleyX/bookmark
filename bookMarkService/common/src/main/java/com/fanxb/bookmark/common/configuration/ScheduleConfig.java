5e12b5a8b17c10668b5510f2d1efeb61d58f6c24
fanxb <fleyx20@outlook.com>
2022-07-18 20:25:24 +0800

    fix:修复定时任务线程池不起作用

--- bookMarkService/common/src/main/java/com/fanxb/bookmark/common/configuration/ScheduleConfig.java
+++ bookMarkService/common/src/main/java/com/fanxb/bookmark/common/configuration/ScheduleConfig.java
@@ -1,6 +1,9 @@
 package com.fanxb.bookmark.common.configuration;
 
 import com.fanxb.bookmark.common.factory.CustomThreadFactory;
+import com.fanxb.bookmark.common.factory.ThreadPoolFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
@@ -11,15 +14,15 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
  * Created with IntelliJ IDEA
  *
  * @author fanxb
- * @date 2020/1/26
  */
+@Configuration
+@Slf4j
 public class ScheduleConfig implements SchedulingConfigurer {
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
-
-        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(5, new CustomThreadFactory("schedule"));
-        scheduledTaskRegistrar.setScheduler(service);
+        scheduledTaskRegistrar.setScheduler(ThreadPoolFactory.createPool("schedule"));
+        log.info("自定义schedule线程池成功");
     }
 
 }
